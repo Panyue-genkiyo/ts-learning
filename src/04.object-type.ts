1f930ed28132f3d28896cfe2ed1399056fc57cde
panyue <90938238g@gmail.com>
2021-10-15 16:11:22 +0800

    补充ts中对象类型

--- src/04.object-type.ts
+++ src/04.object-type.ts
@@ -14,5 +14,81 @@ const obj3: object = Array;
 const obj4: { foo: number, bar: string} = {foo:1, bar:'hello'}; //限制对象类型只用{}
 
 
-
 // const obj1: object = 1,2,'ss'
+
+let a: object; //一切对象
+
+a = {};
+a = () => {}
+a = []
+
+//{} 用来指定对象中可以包含哪些属性 {属性名:属性值....}
+let b: {} = {};
+// b.name = 'panyue'; //错误
+
+// let c: {name: string} = {}; //错误注意格式严格保持唯一 (多少都不行)
+
+let d: {name: string} = {
+    name: 'panyue'
+};
+
+let f: { name: string, age?:number } = {
+    name: 'panyue' //?表示可选参数
+}
+
+let x: {name: string, [key: string]: any}; //任意类型的属性
+
+x = {
+    name: 'panyue',
+    a:1,
+    c:true,
+    d:() => {},
+    gender: '男'
+}
+
+// let d: Function; //单就这么写没什么意义,与d:object差不多
+//设置函数结构的类型声明
+let ss: (a: number, b: number) => number;
+
+ss = function (n1: number, n2: number) : number {
+    return n1 + n2
+}
+
+//数组
+let df: number[] = [1,23,34,4];
+let dssds: Array<number | string> = [2,3,4,45,'dede', 'helo'];
+
+
+
+//元组长度固定
+//[类型,...]
+let t: [string, string] = ['hello','striong'];
+t = ["dd","ddd"];
+
+//枚举
+//列举所有可能的情况
+
+enum Gender{
+    Male,
+    FeMale
+}
+
+let i : {name: string, gender: Gender} =  {
+    name: 'panyue',
+    gender: Gender.Male
+}
+
+console.log(i.gender === Gender.Male); //true
+
+
+//&表示同时 ｜表示或
+let j: string | number;
+let j2: {name: string} & {age: number} //要同时满足
+
+//类型别名
+let k = 1 | 2 | 3 | 4 | 5 | 6;
+
+type myType = string;
+type OP = 1 | 2 | 3 | 4 | 5 | 6; //提炼出type
+
+// let sssss: OP  = 9

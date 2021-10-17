3bbb1169ea141ae637c1cb9537dde0af88cbba3a
panyue <90938238g@gmail.com>
2021-10-17 15:30:48 +0800

    补充类

--- src/05.class.ts
+++ src/05.class.ts
@@ -284,3 +284,39 @@ const mc = new MyClass<string>('hello');
      console.log(obj); //{a: 1, b:1} Object.assign(a,b);
 })()
 
+
+//类的私有字段
+/**
+ 私有字段以 # 字符开头，有时我们称之为私有名称；
+ 每个私有字段名称都唯一地限定于其包含的类；
+ 不能在私有字段上使用 TypeScript 可访问性修饰符（如 public 或 private）；
+ 私有字段不能在包含的类之外访问，甚至不能被检测到。
+ */
+class  Test {
+    readonly name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    great(){
+        console.log(`hello, my name is ${this.name}`);
+    }
+}
+
+console.log(new Test('pay').name);
+
+//类方法的重载
+class ProductService {
+    getProducts(): void;
+    getProducts(id: number): void;
+    getProducts(id?: number) {
+        if(typeof id === 'number') {
+            console.log(`获取id为 ${id} 的产品信息`);
+        } else {
+            console.log(`获取所有的产品信息`);
+        }
+    }
+}
+
+const productService = new ProductService();
+productService.getProducts();
+productService.getProducts(1);

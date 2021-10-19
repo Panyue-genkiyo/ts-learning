13d10af465bf8c3abffc0a74ecbe4f61b32657ce
panyue <90938238g@gmail.com>
2021-10-19 21:58:17 +0800

    装饰器复用

--- src/12.decorate.ts
+++ src/12.decorate.ts
@@ -34,4 +34,29 @@ console.log(( <any>player ).getPosition());
 console.log(( <any>player ).name);
 
 
+//消息处理可以变为装饰器来使用
+//构造函数类型为参数(class就是构造函数function的语法糖)
+const MessageDecorator = (target: Function) => {
+    target.prototype.message = (content: string) => {
+        console.log(content);
+    }
+}
+@MessageDecorator
+class LoginController{
+    public login(){
+        console.log('登录成功!!');
+        (<any>this).message('loding success msg');
+    }
+}
 
+new LoginController().login();
+
+//复用装饰器,继承的另一条出路
+@MessageDecorator
+class ArticleController{
+    public store(){
+       (this as any).message('保存成功!!');
+    }
+}
+
+new ArticleController().store();

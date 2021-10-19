6c042d562f93b8fdd0c2a2812e58040c04c2f3cf
panyue <90938238g@gmail.com>
2021-10-19 21:46:16 +0800

    装饰器的初步使用

---
+++ src/12.decorate.ts
@@ -0,0 +1,34 @@
+//装饰器
+
+//类装饰器(原型)
+const moveDecorate: ClassDecorator = (target: Function) => {
+    //往原型上添加方法
+    target.prototype.name = 'py';
+    target.prototype.getPosition = (): { x:number; y:number} => {
+        return {
+            x: 100,
+            y: 200
+        }
+    }
+}
+
+@moveDecorate
+class Tank{
+    // public getPosition(){
+        
+    // }
+}
+
+const t = new Tank();
+console.log((t as any).getPosition());//在对象原型上的方法
+
+@moveDecorate
+class Player{
+    // public getPosition(){
+
+    // }
+}
+const player = new Player();
+console.log(( <any>player ).getPosition());
+console.log(( <any>player ).name);
+
